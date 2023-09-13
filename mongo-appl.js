1b2c88a5ffe4171cd40e506fe5e1126e1dd8c1d0
Joebye <evgenii.prs@gmail.com>
2023-09-13 14:38:02 +0300

    mongoDb intro

---
+++ mongo-appl.js
@@ -0,0 +1,4 @@
+import MongoConnection from "./domain/MongoConnection.mjs";
+const mongoconnection = new MongoConnection(process.env.ATLAS_URI, "college");
+const studentsMDB = mongoconnection.getCollection("students");
+studentsMDB.find({phone:{$regex:/^050/}}).toArray().then(data => console.log(data));
